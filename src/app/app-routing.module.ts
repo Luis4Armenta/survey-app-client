0beb63e6ef91d762cf0a5aade25566e6604164f3
beku86 <luigiangelxd@gmail.com>
2021-04-25 22:32:07 -0500

    basic structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NewFormComponent } from './new-form/new-form.component';
+import { BoardComponent } from './pages/board/board.component';
+import { CreateUserComponent } from './pages/create-user/create-user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'create-user', component: CreateUserComponent },
+  { path: 'new-form', component: NewFormComponent },
+  { path: '**', component: BoardComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MdbModule } from 'mdb-angular-ui-kit';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { BoardComponent } from './pages/board/board.component';
+import { AboutmeComponent } from './components/shared/aboutme/aboutme.component';
+import { CreateUserComponent } from './pages/create-user/create-user.component';
+import { NewFormComponent } from './new-form/new-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    BoardComponent,
+    AboutmeComponent,
+    CreateUserComponent,
+    NewFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MdbModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
